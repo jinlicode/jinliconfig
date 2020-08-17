009a44d0c698401819ea3782d8e6f5ef3d6d97a9
hhf <5375530@qq.com>
2020-08-18 00:09:53 +0800

    新增生成随机字符串函数

---
+++ class/tools.go
@@ -0,0 +1,22 @@
+package class
+
+import "math/rand"
+
+// RandomString 返回随机字符串
+func RandomString(n int, allowedChars ...[]rune) string {
+	var letters []rune
+	defaultLetters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	if len(allowedChars) == 0 {
+		letters = defaultLetters
+	} else {
+		letters = allowedChars[0]
+	}
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(b)
+}
