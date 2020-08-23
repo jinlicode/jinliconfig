c2f9536302ec863ba803da16485218b1ac188772
hhf <5375530@qq.com>
2020-08-23 21:10:28 +0800

    安全考虑 mysql面板 每一步操作之后都关闭面访

--- manage/mysql.go
+++ manage/mysql.go
@@ -12,8 +12,12 @@ reSelectPhpMyAdmin:
 	switch PhpMyadminSelect {
 	case "开启":
 		class.ExecLinuxCommand("cd " + basepath + " && docker-compose up -d phpmyadmin")
+		fmt.Println("phpmyadmin管理地址：您的服务器IP地址:8080")
+		fmt.Println("mysql用户名：root")
+		fmt.Println("mysql密码：" + class.ReadMysqlRootPassword(basepath))
 		goto reSelectPhpMyAdmin
 	case "查看配置":
+		fmt.Println("phpmyadmin管理地址：您的服务器IP地址:8080")
 		fmt.Println("mysql服务器地址：" + class.ReadMysqlHost(basepath))
 		fmt.Println("mysql用户名：root")
 		fmt.Println("mysql密码：" + class.ReadMysqlRootPassword(basepath))
@@ -21,6 +25,7 @@ reSelectPhpMyAdmin:
 	case "退出":
 		class.ExecLinuxCommand("cd " + basepath + " && docker-compose stop phpmyadmin")
 	case "返回上一层":
+		class.ExecLinuxCommand("cd " + basepath + " && docker-compose stop phpmyadmin")
 		return false
 	default:
 		class.ExecLinuxCommand("cd " + basepath + " && docker-compose stop phpmyadmin")
