af91d683309bfdd0726a0eaa2ddb3c2adbf1ac4e
hhf <5375530@qq.com>
2020-08-23 16:02:29 +0800

    新增从compose 读取 mysql host信息 以及读取网站各自的mysql信息

--- class/tools.go
+++ class/tools.go
@@ -2,6 +2,7 @@ package class
 
 import (
 	"math/rand"
+	"regexp"
 	"time"
 )
 
@@ -35,6 +36,57 @@ func ReadMysqlRootPassword(basepath string) string {
 		MysqlPassword := DockerComposeYamlMap["services"].(map[string]interface{})["mysql"].(map[string]interface{})["environment"].(map[string]interface{})["MYSQL_ROOT_PASSWORD"]
 
 		return MysqlPassword.(string)
+	}
+	return ""
+}
+
+// ReadMysqlHost  读取compose文件中的mysql host
+func ReadMysqlHost(basepath string) string {
+	//先检测是否存在yaml文件
+	if CheckFileExist(basepath + "docker-compose.yaml") {
+		DockerComposeYamlRead := ReadFile(basepath + "docker-compose.yaml")
+		DockerComposeYamlMap := YamlFileToMap(DockerComposeYamlRead)
+		MysqlHost := DockerComposeYamlMap["services"].(map[string]interface{})["mysql"].(map[string]interface{})["networks"].(map[string]interface{})["jinli_net"].(map[string]interface{})["ipv4_address"]
+
+		return MysqlHost.(string)
+	}
+	return ""
+}
+
+// ReadSiteMysqlInfo  读取compose文件中的网站 mysql 信息
+func ReadSiteMysqlInfo(basepath string, dockerName string, readType string) string {
+	//先检测是否存在yaml文件
+	if CheckFileExist(basepath + "docker-compose.yaml") {
+		DockerComposeYamlRead := ReadFile(basepath + "docker-compose.yaml")
+		DockerComposeYamlMap := YamlFileToMap(DockerComposeYamlRead)
+		bb := DockerComposeYamlMap["services"].(map[string]interface{})[dockerName].(map[string]interface{})
+		mapJSON, err := MapToJson(bb)
+		if err == nil {
+			if readType == "host" {
+				parttern := `(?U)"MYSQL_HOST=(.*)"`
+				r := regexp.MustCompile(parttern)
+				matchs := r.FindStringSubmatch(mapJSON)
+				if len(matchs) == 2 {
+					return matchs[1]
+				}
+			} else if readType == "pass" {
+				parttern := `(?U)"MYSQL_PASS=(.*)"`
+				r := regexp.MustCompile(parttern)
+				matchs := r.FindStringSubmatch(mapJSON)
+				if len(matchs) == 2 {
+					return matchs[1]
+				}
+			} else if readType == "user" {
+				parttern := `(?U)"MYSQL_USER=(.*)"`
+				r := regexp.MustCompile(parttern)
+				matchs := r.FindStringSubmatch(mapJSON)
+				if len(matchs) == 2 {
+					return matchs[1]
+				}
+			}
+
+			return ""
+		}
 	}
 	return ""
 }
